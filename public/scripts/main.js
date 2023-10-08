de0c84c2eca56918299982cb3638a5cee6364f69
Ferner <fernerme@rose-hulman.edu>
2023-10-08 17:35:51 -0400

    added photos and photo-changing effect

---
+++ public/scripts/main.js
@@ -0,0 +1,40 @@
+/** namespace. */
+var rhit = rhit || {};
+
+/* globals */
+rhit.imageIndex = 0;
+rhit.intervalID = null;
+
+
+rhit.handler = function() {
+    if (window.location.pathname.includes("index")) {
+        const imgs = document.querySelectorAll(".about-image"); //select all images
+        const newIndex = (rhit.imageIndex + 1) % imgs.length;
+
+        imgs.forEach((img) => {
+            console.log(img.dataset.imgIndex);
+            if (newIndex == parseInt(img.dataset.imgIndex)) {
+                img.classList.remove("off-image");
+            } else if (rhit.imageIndex == parseInt(img.dataset.imgIndex)) {
+                img.classList.add("off-image");
+            }
+		});
+        rhit.imageIndex = newIndex;
+    } else {
+        if (rhit.intervalID) {
+            clearInterval(rhit.intervalID);
+        }
+        rhit.intervalID = null;
+    }
+}
+
+
+/* Main */
+rhit.main = function () {
+    const imgs = document.querySelectorAll(".about-image");
+    if (!rhit.intervalID) {
+        rhit.intervalID = setInterval(rhit.handler, 4000);
+    }
+};
+
+rhit.main();
